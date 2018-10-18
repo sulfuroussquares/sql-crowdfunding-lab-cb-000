bf6733f40fa383b05eaf78ab85b59fdfedd84d45
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-18 17:08:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, sum(pledges.amount)
 FROM users
-JOIN pledges on user.id = pledges.user_id
+JOIN pledges on users.id = pledges.user_id
 GROUP BY users.name
 ORDER BY users.name"
 end
