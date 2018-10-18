e2b2b8e3e79d784c786254cea7210fe3a52bec06
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-18 16:57:09 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,8 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, pledges.amount
 FROM projects, pledges
-JOIN projects
-ON projects.id = pledges.project_id"
+JOIN projects ON projects.id = pledges.project_id"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
