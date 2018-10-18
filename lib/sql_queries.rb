00edda8aba8a8dcd19fda5215f3fcc5cfe4575a7
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-18 16:57:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, pledges.amount
 FROM projects, pledges
-LEFT OUTER JOIN projects
+JOIN projects
 ON projects.id = pledges.project_id"
 end
 
