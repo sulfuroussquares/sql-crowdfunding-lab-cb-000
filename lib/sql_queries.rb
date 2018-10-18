7b26a8eb945ecd66b0febd994dc097e8f6f615a2
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-18 17:25:23 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 FROM users
 JOIN pledges on users.id = pledges.user_id
 GROUP BY users.name
-ORDER by pledges.amount"
+ORDER by SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
