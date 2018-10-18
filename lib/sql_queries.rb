746982a329ba9868f48848e2c3d959fe1ca88f9b
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-18 17:19:44 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,10 +23,10 @@ ORDER BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM((amount)-projects.funding_goal)
+"SELECT projects.title, SUM((amount)-projects.funding_goal) AS amount_over
 FROM projects
 JOIN pledges ON projects.id = pledges.project_id
-GROUP BY projects.title HAVING SUM(pledges.amount >= projects.funding_goal)"
+GROUP BY projects.title HAVING amount_over > 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
