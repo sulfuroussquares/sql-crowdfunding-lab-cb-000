d3567df32788944bf19cabc2007266daa56402b8
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-18 17:24:08 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,11 @@ HAVING amount_over >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount)"
+"SELECT users.name, SUM(pledges.amount)
+FROM users
+JOIN pledges on users.id = pledges.user_id
+GROUP BY users.name
+ORDER by pledges.amount"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
